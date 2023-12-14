dd99888d7608a837744e6b41a465621e0feaf355
AshutoshNagrale <ashutoshnagrale77@gmail.com>
2023-12-14 19:18:50 +0530

    new changes

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Card from "../components/Card";
+import axios from "axios";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Home = ({ type }) => {
+  console.log("Homepage");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await axios.get("/api/video/" + type);
+      setVideos(res.data);
+    };
+    fetchVideos();
+  }, [type]);
+
+  return (
+    <Container>
+      {videos.map((v) => {
+        return <Card key={v._id} video={v} />;
+      })}
+    </Container>
+  );
+};
+
+export default Home;
